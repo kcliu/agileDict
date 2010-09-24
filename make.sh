ff47a291de968771cf242f82ee43090c64feff5f
kcliu <kcliu@aol.tw>
2010-09-24 02:27:38 -0400

    including components path in the make.sh

--- make.sh
+++ make.sh
@@ -2,5 +2,5 @@
 #pushd chrome/chromeFiles
 #zip -r addons.jar content/ skin/
 #popd
-zip addons.xpi install.rdf chrome.manifest chrome/chromeFiles/* -r
+zip addons.xpi install.rdf chrome.manifest chrome/chromeFiles/* components/* -r
 #rm chrome/chromeFiles/vocab.jar
